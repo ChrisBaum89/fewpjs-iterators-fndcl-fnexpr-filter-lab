d9bc5c2fe7b4cbfc59b0b1be3837b6260658d182
Chris Baum <Christopher.Baum@valpo.edu>
2022-03-08 21:29:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching (drivers, string){
+  let matchedDrivers = drivers.filter( driver => driver.toUpperCase() == string.toUpperCase())
+  return matchedDrivers
+}
+
+function fuzzyMatch (drivers, string){
+  let matchedDrivers = drivers.filter( driver => driver.toUpperCase().indexOf(string.toUpperCase()) === 0)
+  return matchedDrivers
+}
+
+function matchName (drivers, name){
+  let matchedDrivers = drivers.filter( driver => driver.name == name)
+  return matchedDrivers
+}
